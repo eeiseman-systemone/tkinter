b0b20e1167b9d4b379cbfe63a67eaf4ec0de24fa
Eric Eiseman <eeiseman@eaeconsulting.us>
2020-09-23 06:31:51 -0600

    .

--- base.py
+++ base.py
@@ -5,8 +5,19 @@ root = Tk()
 root.title("Learn Tkinter")
 root.iconbitmap("icon.ico")
 
+# Image needs to be global to work inside a function with tkinter windows
+my_img = ImageTk.PhotoImage(Image.open("images/aspen.png"))
 
+def open():
+  global my_img
+  top = Toplevel()
+  top.title("My Second Window")
+  top.iconbitmap("icon.ico")
+  my_label = Label(top,image=my_img).pack()
+  lbl = Label(top, text="Hellow World!").pack()
+  btn2 = Button(top, text="Close Window", command=top.destroy).pack()
 
+btn = Button(root, text="Open Second Window", command=open).pack()
 
 
 
